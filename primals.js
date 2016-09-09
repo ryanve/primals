1b45bba41553c83a8047be18a8cb3e874704a6d5
Ryan Van Etten <ryanve@gmail.com>
2016-09-09 00:07:42 -0700

    reorder checks

--- primals.js
+++ primals.js
@@ -4,11 +4,12 @@
 
   function primals(value) {
     if (typeof value != 'string' || !value || anySpace.test(value)) return value
+    if ('undefined' === value) return void 0
     if ('false' === value) return false
     if ('true' === value) return true
     if ('null' === value) return null
     if (+value || +value === 0 || 'NaN' === value) return +value
-    return 'undefined' === value ? void 0 : value
+    return value
   }
 
   if (typeof module != 'undefined' && module.exports) module.exports = primals
